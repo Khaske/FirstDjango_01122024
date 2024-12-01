a9de5afcffc97aaa9dc299efd379dc91253fd557
Renat Iskhakov <khaske@ya.ru>
2024-12-01 17:00:06 +0300

    Done. M1.task_2

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -20,4 +20,6 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.home),
+    path('about', views.about),
+    path('items/<int:item_id>', views.get_items),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -3,9 +3,32 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+items = [
+    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+    {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+    {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+    {"id": 7, "name": "Картофель фри" ,"quantity":0},
+    {"id": 8, "name": "Кепка" ,"quantity":124},
+]
+
+
+def get_items(request, item_id):
+    # пройти циклом
+    return HttpResponse(items[item_id])
+
 def home(request):
     text = """
     <h1>"Изучаем django"</h1>
     <strong>Автор</strong>: <i>Исхаков Р.Н.</i>
+    """
+    return HttpResponse(text)
+
+def about(request):
+    text = """
+    Имя: <strong>Ренат</strong><br/>
+    Отчество: <strong>Наильевич</strong><br/>
+    Фамилия: <strong>Исхаков</strong><br/>
+    телефон: <strong>+7-000-000-00-00</strong><br/>
+    email: <strong>khaske@ya.ru</strong>
     """
     return HttpResponse(text)
