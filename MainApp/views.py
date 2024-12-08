065ce9630b31be8eeced54825b4d4e98ec41f93b
Renat Iskhakov <khaske@ya.ru>
2024-12-08 15:16:47 +0300

    Done. M2

--- MainApp/views.py
+++ MainApp/views.py
@@ -13,11 +13,11 @@ items = [
 
         
 def home(request): # M1.task_1
-    text = """
-    <h1>"Изучаем django"</h1>
-    <strong>Автор</strong>: <i>Исхаков Р.Н.</i>
-    """
-    return HttpResponse(text)
+    context = {
+        "name": "Исхаков Ренат Наильевич",
+        "email": "khaske@ya.ru"
+    }
+    return render(request, "index.html", context)
 
 def about(request): # M1.task_2, лучше сделать через словарь
     text = """
@@ -29,27 +29,38 @@ def about(request): # M1.task_2, лучше сделать через слова
     """
     return HttpResponse(text)
 
-def get_item(request, item_id): # M1.task_3&4&6
-    for item in items:
-        if item["id"] == item_id:
-            result = f"""
-            <p>Товар с id={item["id"]}: {item["name"]}, {item["quantity"]} шт.</p>
-            <a href="/items">Назад к списку товаров</a>
-            """
-            return HttpResponse(result)
-    else:
-        result = f"""
-        <p>Товар с id={item_id}: не найден!</p>
-        <a href="/items">Назад к списку товаров</a>
-        """
-        return HttpResponseNotFound(result)
+def get_item(request, item_id: int): # M1.task_3&4&6
+    # for item in items:
+    #     if item["id"] == item_id:
+    #         result = f"""
+    #         <p>Товар с id={item["id"]}: {item["name"]}, {item["quantity"]} шт.</p>
+    #         <a href="/items">Назад к списку товаров</a>
+    #         """
+    #         return HttpResponse(result)
+    # else:
+    #     result = f"""
+    #     <p>Товар с id={item_id}: не найден!</p>
+    #     <a href="/items">Назад к списку товаров</a>
+    #     """
+    #     return HttpResponseNotFound(result)
+    item = next((item for item in items if item["id"] == item_id), None) # генератор
+    if item is not None:
+        context = {
+            "item": item
+        }
+        return render(request, "item.html", context)
+    return HttpResponseNotFound(f"Товар с id = {item_id} не найден!")
     
 def get_items_list(request): # M1.task_5&6
-    list = """
-    <strong>Список всех товаров:</strong>
-    <ol>\n
-    """
-    for item in items:
-        list += f'  <li><a href="/items/{item["id"]}">{item["name"]}, {item["quantity"]} шт.</a></li>\n'
-    list += "</ol>"
-    return HttpResponse(list)
+    # list = """
+    # <strong>Список всех товаров:</strong>
+    # <ol>\n
+    # """
+    # for item in items:
+    #     list += f'    <li><a href="/items/{item["id"]}">{item["name"]}, {item["quantity"]} шт.</a></li>\n'
+    # list += "</ol>"
+    # return HttpResponse(list)
+    context = {
+        "items": items
+    }
+    return render(request, "items_list.html", context)
--- README.md
+++ README.md
@@ -9,3 +9,19 @@
 ## Получение репозитория
 1. git init # в папке проекта
 2. git pull origin master
+
+## Дополнительно
+1. Git Graph
+2. Полезное дополнение для шаблонов 'Django'
+```
+ext install batisteo.vscode-django
+```
+3. Добавить в `settings.json` (нажать значек страницы)
+```
+"emmet.includeLanguages": {
+        "django-html": "html",
+    },
+"files.associations": {
+    "*.html": "django-html"
+}
+```
